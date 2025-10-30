726218d35d13d10b24b15bae0fa4b501e4a9c896
khanh gia <luonggiakhanh1997@gmail.com>
2025-10-30 18:47:40 +0700

    add nofitication

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  controllers: [CommentsController],
-  providers: [CommentsService]
+  imports: [PrismaModule, NotificationsModule],
+  providers: [CommentsService],
+  exports: [CommentsService],
 })
 export class CommentsModule {}
